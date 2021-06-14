ad80fd60053b324c480be6a5f84798530490e6c6
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-06-14 09:37:13 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,8 +1,22 @@
-public class EmployeeWage{
- 
-public static void main(String args[]){
- System.out.println("WELCOME TO EMPLOYEEWAGE");
- System.out.println("Added 2nd use case");
-}
 
-}
+public class EmpwageBuilderUC2
+{
+  public static void main(String args[])
+  {
+     int IS_FULL_TIME=1;
+     int EMP_RATE_PER_HOUR=20;
+     int empHrs=0;
+     int empWage=0;
+  duoble empcheck=Math.floor(Math.random()*10)%2;
+   if(empcheck==IS_FULL_TIME)
+    {
+      empHrs=8;
+    }
+   else
+   {
+      empHrs=0;
+   }
+  empWage=empHrs*EMP_RATE_PER_HOUR;
+  System.out.println("empwage" +empWage);
+  }
+} 
