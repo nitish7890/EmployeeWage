d7a1fde890e5a15f754ff7057f854ea6f00a771f
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-06-14 09:35:58 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,17 @@
 public class EmployeeWage{
  
-public static void main(String args[]){
- System.out.println("WELCOME TO EMPLOYEEWAGE");
-}
-
-}
+{
+  public static void main(String args[])
+  {
+     int IS_FULL_TIME=1;
+  duoble empcheck=Math.floor(Math.random()*10)%2;
+   if(empcheck==IS_FULL_TIME)
+    {
+    System.out.println("employee is present")
+    }
+   else
+   {
+  System.out.println("employee is absent")
+   }
+  }
+} 
