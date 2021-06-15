6516b72675f18429349373f5038553484c20f05f
Nitish <nitishmehta1997@gmail.com>
2021-06-15 16:05:12 +0530

    added fourth use case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,20 +1,25 @@
-public class EmployeeWage
+public class EmpwageBuilderUC2
 {
+  public static final int IS_PART_TIME=1;
+  public static final int IS_FULL_TIME=2;
+  public static final int EMP_RATE_PER_HOUR=20;
   public static void main(String args[])
   {
-     int IS_PART_TIME=1
-     int EMP_RATE_PER_HOUR=20;
      int empHrs=0;
      int empWage=0;
-  duoble empcheck=Math.floor(Math.random()*10)%2;
-   if(empcheck==IS_PART_TIME)
+  int empcheck=(int)Math.floor(Math.random()*10)%3;
+   switch(empcheck)
     {
-      empHrs=8;
+      case IS_PART_TIME:
+        empHrs=4;
+        break;
+      case IS_FULL_TIME: 
+        empHrs=8;
+        break;
+      default:
+          empHrs=0;
     }
-   else
-   {
-      empHrs=0;
-   }
+   
   empWage=empHrs*EMP_RATE_PER_HOUR;
   System.out.println("empwage" +empWage);
   }
