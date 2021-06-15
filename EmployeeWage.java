9c9caf9ce3ca4c0553965206827af8fc2adb5930
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-06-15 16:10:29 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,19 @@
+
 public class EmpwageBuilderUC2
 {
   public static final int IS_PART_TIME=1;
   public static final int IS_FULL_TIME=2;
   public static final int EMP_RATE_PER_HOUR=20;
+  public static final int NUM_OF_WORKING_DAY=2;
+  public static final int MAX_HRS_IN_WORKING=10;
   public static void main(String args[])
   {
      int empHrs=0;
-     int empWage=0;
+     int totalEmpHrs=0;
+     int totalWorkingDays=0;
+  while(totalEmpHrs<=NUM_OF_WORKING_DAY)
+   {
+     totalWorkingDays++;
   int empcheck=(int)Math.floor(Math.random()*10)%3;
    switch(empcheck)
     {
@@ -19,11 +26,12 @@ public class EmpwageBuilderUC2
       default:
           empHrs=0;
     }
-   
-  empWage=empHrs*EMP_RATE_PER_HOUR;
-  System.out.println("empwage" +empWage);
+   totalEmpHrs+=empHrs;
+   System.out.println("Days" +totalWorkingDays+"EmpHrs"+empHrs);
   }
-} 
-
+  int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+  System.out.println("total emp wage" +totalEmpWage);
+  }
+ }
 
 
